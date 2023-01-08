c0a7639e3920ce8333a073f41035c876b89d8a19
kshopov <krasimir.shopov@gmail.com>
2023-01-08 15:51:46 +0200

    book controller created

--- ch_02/spring5webapp/src/main/java/com/kshopov/spring5webapp/controllers/BookController.java
+++ ch_02/spring5webapp/src/main/java/com/kshopov/spring5webapp/controllers/BookController.java
@@ -1,2 +1,24 @@
-package com.kshopov.spring5webapp.controllers;public class BookController {
+package com.kshopov.spring5webapp.controllers;
+
+import com.kshopov.spring5webapp.repository.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBook(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
+
 }
