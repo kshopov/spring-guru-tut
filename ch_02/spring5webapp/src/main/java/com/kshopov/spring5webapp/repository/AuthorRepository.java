7e07d321ba2548f9e9158fe8ba548dd91acb8170
kshopov <krasimir.shopov@gmail.com>
2023-01-08 11:41:01 +0200

    added boot and author repositories

---
+++ ch_02/spring5webapp/src/main/java/com/kshopov/spring5webapp/repository/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.kshopov.spring5webapp.repository;
+
+import com.kshopov.spring5webapp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
