7d6609f9f478cef43d4f7178246c413383fe3d51
kshopov <krasimir.shopov@gmail.com>
2023-01-08 21:17:36 +0200

    added dependency injection project

---
+++ ch_03/dependency-injection/src/test/java/com/kshopov/dependencyinjection/DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kshopov.dependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
