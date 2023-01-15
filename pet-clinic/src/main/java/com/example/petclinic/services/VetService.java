547663913c6d11290b67cea202939922195a2d28
kshopov <krasimir.shopov@gmail.com>
2023-01-15 17:04:22 +0200

    changed crud repository to crud service

--- pet-clinic/src/main/java/com/example/petclinic/services/VetService.java
+++ pet-clinic/src/main/java/com/example/petclinic/services/VetService.java
@@ -1,7 +1,6 @@
 package com.example.petclinic.services;
 
 import com.example.petclinic.model.Vet;
-import org.springframework.data.repository.CrudRepository;
 
-public interface VetService extends CrudRepository<Vet, Long> {
+public interface VetService extends CrudService<Vet, Long> {
 }
